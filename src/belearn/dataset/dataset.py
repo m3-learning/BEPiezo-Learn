07010733e2952288eb5473ff7ec8ace106fc47b6
Julian Goddy <jg3837@drexel.edu>
2025-04-29 13:15:35 -0400

    The SHO_preprocessing somehow works after redownloading the data but I had to comment out the check that the data already exists in generate_noisy_data

--- src/belearn/dataset/dataset.py
+++ src/belearn/dataset/dataset.py
@@ -428,12 +428,12 @@ class BE_Dataset(BE_DataFed):
         with h5py.File(self.file, "r+") as h5_f:
             # Iterate through each noise level provided in the list
             for noise_level in noise_levels:
-                if (
-                    usid.hdf_utils.find_dataset(h5_f, f"Noisy_Data_{noise_level}")
-                    is not []
-                ):
-                    print(f"Noisy_Data_{noise_level} already exists")
-                    continue
+                # if (
+                #     usid.hdf_utils.find_dataset(h5_f, f"Noisy_Data_{noise_level}")
+                #     is not []
+                # ):
+                #     print(f"Noisy_Data_{noise_level} already exists")
+                #     continue
 
                 if verbose:
                     print(f"Adding noise level {noise_level}")

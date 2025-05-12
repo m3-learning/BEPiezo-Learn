moved SHO_Fit_comparison from viz to state, since it doesn't actually plot anything. However, somehow the get_best_median_worst is giving incorrect results

src/belearn/viz/viz.py
@@ -5,8 +5,8 @@ from typing import List, Dict, Optional, Any, Type, Callable
 from belearn.dataset.dataset import BE_Dataset
 from belearn.dataset.model_utils import BE_model_utils
 from belearn.util.wrappers import context_manager_decorator
-from belearn.dataset.analytics import get_rankings, MSE
-from belearn.dataset.transformers import to_real_imag, to_complex
+from belearn.dataset.analytics import MSE
+#from belearn.dataset.transformers import to_real_imag, to_complex
 
 import numpy as np
 import matplotlib.pyplot as plt
@@ -1717,213 +1717,7 @@ class Viz(BE_model_utils):
             if kwargs["returns"] == True:
                 return d1, d2, index1, mse1
 
-    def get_best_median_worst(
-        self,
-        true_state,
-        prediction=None,
-        out_state=None,
-        n=1,
-        SHO_results=False,
-        index=None,
-        compare_state=None,
-        fit_type="SHO",
-        **kwargs,
-    ):
-        def data_converter(data):
-            # converts to a standard form which is a list
-            data = to_real_imag(data)
-
-            try:
-                # converts to numpy from tensor
-                data = [data.numpy() for data in data]
-            except:
-                pass
-
-            return data
-
-        if fit_type == "SHO":
-            if type(true_state) is dict:
-                self.set_attributes(**true_state)
-
-                # the data must be scaled to rank the results
-                self.scaled = True
-
-                true, x1 = self.raw_spectra(frequency=True)
-
-            elif isinstance(true_state, (torch.Tensor, np.ndarray, list)):
-                true_state = (
-                    true_state.numpy()
-                    if isinstance(true_state, torch.Tensor)
-                    else true_state
-                )
-                true = data_converter(true_state)
-
-                # gets the frequency values
-
-                if true[0].ndim == 2:
-                    x1 = self.get_freq_values(true[0].shape[1])
-
-            # # condition if x_data is passed
-            # elif np.iscomplex(true_state).all():
-            #     true = data_converter(true_state)
-
-            #     # gets the frequency values
-            #     if true[0].ndim == 2:
-            #         x1 = self.get_freq_values(true[0].shape[1])
-            else:
-                raise ValueError(
-                    "true_state must be a dictionary, torch.Tensor, np.ndarray, or list"
-                )
-        elif fit_type == "hysteresis":
-            # gets the true data
-
-            # gets the x values
-            data, voltage = self.get_hysteresis(scaled=True, loop_interpolated=True)
-
-            x1 = self.get_voltage
-
-        # holds the raw state
-        current_state = self.get_state
-        # LSQF_phase_shift is pi/2 here and 0 on Shenron
-
-        if isinstance(prediction, nn.Module):
-            fitter = "NN"
-
-            if fit_type == "SHO":
-                # sets the phase shift to zero for parameters
-                # This is important if doing the fits because the fits will be wrong if the phase is shifted.
-                self.NN_phase_shift = 0
-                self.LSQF_phase_shift = 0  # ********
-
-                data = self.to_nn(true)
-
-                pred_data, scaled_params, params = prediction.predict(data)
-
-                self.scaled = True
-
-                prediction, x2 = self.raw_spectra(
-                    fit_results=params, frequency=True, scaled=self.scaled
-                )
-            elif fit_type == "hysteresis":
-                pred_data, scaled_params, params = prediction.predict(
-                    torch.tensor(data.reshape(-1, 96, 1)), is_SHO=False
-                )
-                x2 = self.get_voltage
-
-                self.scaled = True
-
-            # prediction, x2 = self.dataset.raw_spectra(
-            #     fit_results=params, voltage_step = self.get_voltage_step(), frequency=True
-            # )
-
-            # prediction, embedding = self.model(data) #or maybe true_state
-            # prediction = prediction.to(torch.float32)
-            # prediction = prediction.reshape(prediction.shape[0],prediction.shape[1],1)
-
-        elif isinstance(prediction, dict):
-            fitter = prediction["fitter"]
-
-            exec(f"self.{prediction['fitter']}_phase_shift =0")
-
-            self.scaled = False
-
-            params = self.SHO_fit_results()
-
-            params = params.reshape(-1, 4)
-
-            self.scaled = True
-
-            prediction, x2 = self.raw_spectra(
-                fit_results=params,
-                voltage_step=self.get_voltage_step(),
-                frequency=True,
-                scaled=self.scaled,
-            )
-
-        if "x2" not in locals():
-            # if you do not use the model will run the
-            x2 = self.get_freq_values(prediction[0].shape[1])
-
-        # index the data if provided
-        if index is not None:
-            true = [true[0][index], true[1][index]]
-            prediction = [prediction[0][index], prediction[1][index]]
-            # params = params[index]
-
-        if compare_state is not None:
-            compare_state = data_converter(compare_state)
-
-            # this must take the scaled data
-            index1, mse1, d1, d2 = get_rankings(compare_state, prediction, n=n)
-        else:
-            # this must take the scaled data
-            if fit_type == "SHO":
-                full_indices, index1, mse1, d1, d2 = get_rankings(true, prediction, n=n)
-
-            elif fit_type == "hysteresis":
-                full_indices, index1, mse1, d1, d2 = get_rankings(
-                    torch.tensor(data).reshape(-1, 96, 1),
-                    pred_data,
-                    n=n,
-                    fit_type="hysteresis",
-                )
-            # index1, mse1, d1, d2 = get_rankings(data, pred_data.reshape(60,60,4,96), n=n)
-
-        d1, labels = self.out_state(d1, out_state)
-        d2, labels = self.out_state(d2, out_state)
-
-        # saves just the parameters that are needed
-        params = params[index1]
-
-        # resets the current state to apply the phase shifts
-        self.set_attributes(**current_state)
-
-        # gets the original index values
-        if index is not None:
-            index1 = index[index1]
-
-        # if statement that will return the values for the SHO Results
-        if SHO_results:
-            if eval(f"self.{fitter}_phase_shift") is not None:
-                params[:, 3] = eval(
-                    f"self.shift_phase(params[:, 3], self.{fitter}_phase_shift)"
-                )
-            return (d1, d2, x1, x2, labels, full_indices, index1, mse1, params)
-        else:
-            return (d1, d2, x1, x2, labels, full_indices, index1, mse1)
-
-    # TODO: add comments and docstring
-    def out_state(self, data, out_state):
-        # holds the raw state
-        current_state = self.get_state
-
-        def convert_to_mag(data):
-            data = to_complex(data, axis=1)
-            data = self.raw_data_scaler.inverse_transform(data)
-            data = [
-                np.abs(data),
-                np.angle(data),
-            ]  # this to_magnitude function was only one line so unnecessary to call it? self.to_magnitude(data)
-            data = np.array(data)
-            data = np.rollaxis(data, 0, data.ndim - 1)
-            return data
-
-        labels = ["real", "imaginary"]
-
-        if out_state is not None:
-            if "raw_format" in out_state.keys():
-                if out_state["raw_format"] == "magnitude spectrum":
-                    data = convert_to_mag(data)
-                    labels = ["Amplitude", "Phase"]
-
-            elif "scaled" in out_state.keys():
-                if out_state["scaled"] == False:
-                    data = self.raw_data_scaler.inverse_transform(data)
-                    labels = ["Scaled " + s for s in labels]
-
-        self.set_attributes(**current_state)
-
-        return data, labels
+   
 
     # @static_dataset_decorator
     @context_manager_decorator
@@ -2752,6 +2546,8 @@ class Viz(BE_model_utils):
                     ax_.legend(lines + lines2, labels + labels2, loc="upper right")
 
                 set_sci_notation_label(ax_, axis="x", corner="bottom right")
+                set_sci_notation_label(ax1, axis="y", corner="top left")
+
 
         # Save the figure if filename is provided
         if self.printer is not None and filename is not None:
@@ -3356,26 +3152,26 @@ class Viz(BE_model_utils):
                             cbar.set_label(self.hysteresis_maps_colorbar_labels[col],size=15,loc='center')  # Add a label to the colorbar
                             
                                 
-                        if row == 0: 
-                            labelfigs(ax,
-                                    string_add="Least Squares Fit Method",
-                                    loc='tl',size=25,inset_fraction = (0.05,0.5),style='b',
-                                    horizontalalignment = 'center',verticalalignment='center')
-                            labelfigs(ax,
-                                    string_add="h",
-                                    loc='tl',size=25,inset_fraction = (0.05,0.05),style='b',
-                                    horizontalalignment = 'left',verticalalignment='center')
-                        else:
-                            labelfigs(ax,
-                                    string_add="Neural Network with Trust Region CG",
-                                    loc='tl',size=25,inset_fraction = (0.55,0.5),style='b',
-                                    horizontalalignment = 'center',verticalalignment='center')
-                            labelfigs(ax,
-                                    string_add="i",
-                                    loc='tl',size=25,inset_fraction = (0.55,0.05),style='b',
-                                    horizontalalignment = 'left',verticalalignment='center')
+                        
+                labelfigs(ax,
+                        string_add="Least Squares Fit Method",
+                        loc='tl',size=25,inset_fraction = (0.05,0.5),style='b',
+                        horizontalalignment = 'center',verticalalignment='center')
+                labelfigs(ax,
+                        string_add="h",
+                        loc='tl',size=25,inset_fraction = (0.05,0.05),style='b',
+                        horizontalalignment = 'left',verticalalignment='center')
+            
+                labelfigs(ax,
+                        string_add="Neural Network with Trust Region CG",
+                        loc='tl',size=25,inset_fraction = (0.55,0.5),style='b',
+                        horizontalalignment = 'center',verticalalignment='center')
+                labelfigs(ax,
+                        string_add="i",
+                        loc='tl',size=25,inset_fraction = (0.55,0.05),style='b',
+                        horizontalalignment = 'left',verticalalignment='center')
                             
-                    ax.axis("off")         
+                ax.axis("off")         
             else:
                 ax.axis("off")
                 size=(1.25, 1.25)